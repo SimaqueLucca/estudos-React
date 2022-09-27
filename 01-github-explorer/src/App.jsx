2dd4c2bb3464329769e9e236d9e6972a77e632f1
Luca Simaque Souza <luccasimaque@yahoo.com.br>
2022-09-27 15:36:11 -0300

    chamadas http, repositorios do github

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,13 +1,7 @@
-import { Counter } from "./components/Counter";
 import { RepositoryItem } from "./components/RepositoryItem";
 import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 
 export function App() {
-  return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
-  );
+  return <RepositoryList />;
 }
--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,27 +1,24 @@
 import { RepositoryItem } from "./RepositoryItem";
-
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "github",
-};
+import "../styles/repositories.scss";
+import { useState, useEffect } from "react";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/SimaqueLucca/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem />
-        <RepositoryItem />
-        <RepositoryItem />
-        <RepositoryItem />
-        <RepositoryItem />
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
       </ul>
     </section>
   );
